9e9cec42f50c0556a0558e35218f2c65b01ec22b
kiran-garre <kgarre.cs@gmail.com>
2025-08-12 23:22:56 -0700

    Implement `efree()` for freeing emulated memory
    Updates:
    - Basic `emalloc` and `efree` functions are now ready for testing
    (or using and discovering bugs later)

--- ememory.c
+++ ememory.c
@@ -8,7 +8,7 @@
 #define STARTING_OFFSET		0x10
 
 // Errors
-#define DOUBLE_FREE
+#define DOUBLE_FREE_ERR
 
 struct ememory {
 	char data[MEM_SIZE];
@@ -25,103 +25,196 @@ struct eptr {
 /**
  * Header format:
  * 
- * +++++++++++++++ +++++++++++++++ +++++++++++++++
- * | size (word) | | next (word) | | prev (word) |
- * +++++++++++++++ +++++++++++++++ +++++++++++++++
+ * +++++++++++++++ +++++++++++++++ 
+ * | size (word) | | next (word) |
+ * +++++++++++++++ +++++++++++++++ 
  * 
  * where: 
  * 	- `size` contains the size of the free space (including the header)
  * 	- `next` contains the index of the start of the next free section 
- * 	- `prev` contains the index of the start of the previous free section
  */
 
-void write_header(char* data, uint16_t size, uint16_t next, uint16_t prev, uint16_t offset) {
-	memcpy(data + offset, size, sizeof(uint16_t));
-	memcpy(data + offset + sizeof(uint16_t), next, sizeof(uint16_t));
-	memcpy(data + offset + sizeof(uint16_t) + sizeof(uint16_t), prev, sizeof(uint16_t));
+void write_header(char* data, uint16_t size, uint16_t next, uint16_t offset) {
+	memcpy(data + offset, &size, sizeof(uint16_t));
+	memcpy(data + offset + sizeof(uint16_t), &next, sizeof(uint16_t));
 }
 
-void read_header(char* data, uint16_t* size, uint16_t* next, uint16_t* prev, uint16_t offset) {
+void read_header(char* data, uint16_t* size, uint16_t* next, uint16_t offset) {
 	memcpy(size, data + offset, sizeof(uint16_t));
 	memcpy(next, data + offset + sizeof(uint16_t), sizeof(uint16_t));
-	memcpy(prev, data + offset + sizeof(uint16_t) + sizeof(uint16_t), sizeof(uint16_t));
 }
 
 void init_emalloc(struct ememory* memory) {
-	write_header(memory->data, MEM_SIZE - STARTING_OFFSET, 0, 0, STARTING_OFFSET);
+	write_header(memory->data, MEM_SIZE - STARTING_OFFSET, 0, STARTING_OFFSET);
 	memory->free_head = STARTING_OFFSET;
 }
 
 struct eptr emalloc(struct ememory* memory, uint16_t size) {
 	struct eptr res;
 	char* data = memory->data;
-	uint16_t block_size, next, prev;
+	uint16_t block_size, next;
+	uint16_t prev = 0;
 	uint16_t curr = memory->free_head;
 	
-	while (curr != 0) {
-		read_header(data, &block_size, &next, &prev, curr);
+	while (curr) {
+		read_header(data, &block_size, &next, curr);
 		if (block_size >= size) {
 			res.ptr = curr;
 			res.size = size;
 
 			// Shrink free block if space is left over
-			// Otherwise, update pointer of previous block to point to next
+			// Otherwise, if prev exists, set prev.next = next
+			// Otherwise, set free_head to next
 			if (block_size > size) {
 				curr += size;
-				write_header(data, block_size - size, next, prev, curr);
+				write_header(data, block_size - size, next, curr);
+				if (prev == 0) {
+					memory->free_head = curr;
+				} else {
+					memcpy(data + prev + sizeof(uint16_t), curr, sizeof(uint16_t));
+				}
 			} else if (prev != 0) {
 				memcpy(data + prev + sizeof(uint16_t), next, sizeof(uint16_t));
+			} else {
+				memory->free_head = next;
 			}
 			return res;
 		} 
+		prev = curr;
 		curr = next;
 	}
 	return ENULL;
 }
 
 /**
- * If ptr < free_head:
- * 		free_head = ptr
- * 		if ptr + size == free_head
- *			- write header at ptr with next = free_head.next, size = size + free_head.size
- *		else
- *			- write header at ptr with next = free_head, size = size
- *
- * As we're iterating through the list:
- * - curr == ptr && ptr + size == next
- * 		-> Merge everything into one big chunk
- * 		-> curr.next = next.next
- * 		-> curr.size += ptr.size + next.size
- * - curr < ptr && ptr + size == next
- * 		-> Merge this and next
- * 		-> curr.next = ptr
- * 		-> write header at ptr containing next = next.next, size = size + next.size
- * - curr == ptr && ptr + size < next
- * 		-> Merge prev and this
- * 		-> curr.size += size
- * - prev < ptr && ptr + size < next
- * 		-> Insert without merging
- * 		-> curr.next = ptr
- * 		-> write header at ptr containing next = next, size = size
- * 
- * If we've iterated through the list and haven't returned, we know that it must be a new node
- * at the end.
- * if ptr = last + last.size: 
- * 		last.size += ptr.size
- * else
- * 		last.next = ptr
- * 		write header at ptr with next = 0, size = size
+ * Free a block at ptr in a free list of memory blocks
  */
 int efree(struct ememory* memory, struct eptr ptr) {
 	char* data = memory->data;
-	uint16_t block_size, next, prev;
+	uint16_t curr_size, curr_next, next_size, next_next;
 	uint16_t curr = memory->free_head;
 
-	while (curr != 0) {
-		read_header(memory->data, &block_size, &next, &prev, curr);
-		if (prev < ptr.ptr && ptr.ptr + ptr.size < next) {
-
-		}
+	if (ptr.ptr < curr) {
+		return free_before_head(memory, ptr);
 	}
+
+	while (curr) {
+		read_header(data, &curr_size, &curr_next, curr);
+
+		// Our ptr is somewhere past the last free header
+		if (curr_next == 0) {
+			break;
+		}
+
+		read_header(data, &next_size, &next_next, curr_next);
+		if (curr <= ptr.ptr && ptr.ptr <= curr_next) {
+			return free_in_middle(memory, ptr, curr, curr_size, curr_next, next_size, next_next);
+		}
+		curr = curr_next;
+	}
+
+	return free_at_tail(memory, ptr, curr, curr_size);
 } 
 
+static int free_before_head(struct ememory *memory, struct eptr ptr) {
+    char* data = memory->data;
+    uint16_t head_size, head_next;
+
+    // Read the header of the current head
+    read_header(data, &head_size, &head_next, memory->free_head);
+
+    // If new block is directly before the head, merge
+    if (ptr.ptr + ptr.size == memory->free_head) {
+        write_header(data, ptr.size + head_size, head_next, ptr.ptr);
+    } else {
+        write_header(data, ptr.size, memory->free_head, ptr.ptr);
+    }
+
+    // Update head pointer
+    memory->free_head = ptr.ptr;
+    return 0;
+}
+
+static int free_in_middle(struct ememory *memory, struct eptr ptr,
+                          uint16_t curr, uint16_t curr_size,
+                          uint16_t curr_next, uint16_t next_size,
+                          uint16_t next_next) {
+	char* data = memory->data;
+	if (curr + curr_size == ptr.ptr && ptr.ptr + ptr.size == curr_next) {
+		write_header(data, curr_size + ptr.size + next_size, next_next, curr);
+		return 0;
+	}
+	if (curr + curr_size < ptr.ptr && ptr.ptr + ptr.size == curr_next) {
+		write_header(data, curr_size, ptr.ptr, curr);
+		write_header(data, ptr.size + next_size, next_next, ptr.ptr);
+		return 0;
+	}
+	if (curr + curr_size == ptr.ptr && ptr.ptr + ptr.size < curr_next) {
+		write_header(data, curr_size + ptr.size, curr_next, curr);
+		return 0;
+	}
+	if (curr + curr_size < ptr.ptr && ptr.ptr + ptr.size < curr_next) {
+		write_header(data, curr_size, ptr.ptr, curr);
+		write_header(data, ptr.size, curr_next, ptr.ptr);
+		return 0;
+	}
+}
+
+static int free_at_tail(struct ememory *memory, struct eptr ptr,
+                        uint16_t curr, uint16_t curr_size) {
+    char* data = memory->data;
+	if (curr + curr_size == ptr.ptr) {
+		write_header(data, curr_size + ptr.size, 0, curr);
+	} else {
+		write_header(data, curr_size, ptr.ptr, curr);
+		write_header(data, ptr.size, 0, ptr.ptr);
+	}
+}
+
+
+/**
+ * IMPLEMENTATION DETAILS:
+ * 
+ * efree():
+ * 		Although it looks like a long function, the logic is mostly the same
+ * 		is insertion into a linked list, with some more cases handled when
+ * 		free sections are perfectly adjacent. The linked list is stored as 
+ * 		headers in the memory blocks themselves and can be overwritten after 
+ * 		being allocated.
+ * 		
+ * 		The full implementation logic is below.
+ * 
+ * 		1. If ptr is before the current free head:
+ * 		     a. If ptr + size == free_head:
+ * 		            - Merge with head: write header at ptr with
+ * 		              next = free_head.next, size = size + free_head.size
+ * 		     b. Else:
+ * 		            - Insert before head: write header at ptr with
+ * 		              next = free_head, size = size
+ * 		     c. Update free_head = ptr
+ *		
+ * 		2. While iterating through the free list, for a position where curr <= ptr <= next,
+ * 		   there are four possible cases:
+ * 		     a. curr + curr.size == ptr && ptr + size == next
+ * 		            - Merge prev, ptr, and next into one block
+ * 		            - curr.next = next.next
+ * 		            - curr.size += ptr.size + next.size
+ * 		     b. curr + curr.size < ptr && ptr + size == next
+ * 		            - Merge ptr with next
+ * 		            - curr.next = ptr
+ * 		            - write header at ptr with next = next.next, size = size + next.size
+ * 		     c. curr + curr.size == ptr && ptr + size < next
+ * 		            - Merge curr with ptr
+ * 		            - curr.size += size
+ * 		     d. curr + curr.size < ptr && ptr + size < next
+ * 		            - Insert without merging
+ * 		            - curr.next = ptr
+ * 		            - write header at ptr with next = next, size = size
+ *		
+ * 		3. If iteration reaches the end of the list without returning:
+ * 		     a. If ptr is adjacent to the last block (ptr == last + last.size):
+ * 		            - Merge with last: last.size += ptr.size
+ * 		     b. Else:
+ * 		            - Append new block: last.next = ptr
+ * 		            - write header at ptr with next = 0, size = size
+ */
